2daad99ab40de2c8bfdfe9a3fd775243cedecd7e
Royyan <mproyyan@gmail.com>
2026-08-24 09:04:33 +0700

    Initial Commit

---
+++ Astar/AstarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstarApp.swift
+//  Astar
+//
+//  Created by Muhammad Pandu Royyan on 24/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AstarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstarTests/AstarTests.swift
@@ -0,0 +1,19 @@
+//
+//  AstarTests.swift
+//  AstarTests
+//
+//  Created by Muhammad Pandu Royyan on 24/08/26.
+//
+
+import Testing
+@testable import Astar
+
+struct AstarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
