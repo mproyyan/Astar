c7c86646fd3a0be1ca71f2ddc704f8c508abe9a1
Nadia Lubis <lubisputri16@gmail.com>
2026-08-26 13:03:21 +0700

    (feat.) trusted person view

--- Astar/Features/Profile/TrustedPerson/TrustedPersonView.swift
+++ Astar/Features/Profile/TrustedPerson/TrustedPersonView.swift
@@ -8,38 +8,22 @@
 import SwiftUI
 
 
-struct SampleData: Identifiable {
-    var id: Int
-    var avatar: String
-    var displayName: String
-    var icloud: Int
-}
-
-var sampleData: [SampleData] = [
-    SampleData(id: 1, avatar: "person.crop.circle.fill", displayName: "Alex Morgan", icloud: 128),
-    SampleData(id: 2, avatar: "person.crop.square.fill", displayName: "Jordan Lee", icloud: 512),
-    SampleData(id: 3, avatar: "person.fill.checkmark", displayName: "Taylor Swift", icloud: 2000),
-    SampleData(id: 4, avatar: "person.fill.turn.right", displayName: "Chris Evans", icloud: 50),
-    SampleData(id: 5, avatar: "person.wave.2.fill", displayName: "Sam Wilson", icloud: 200)
-]
-
-var firstData: SampleData {
-    sampleData.first!
-}
-
-
 struct TrustedPersonView: View {
     var body: some View {
         NavigationStack {
             
             
-            RequestSection()
-            
-            TrustedPersonList()
+            ScrollView {
+                RequestSection()
+                
+                TrustedPersonList()
+            }
+            .padding(16)
             
             
                 .navigationTitle("Trusted Person")
-                .navigationBarTitleDisplayMode(.large)
+                .navigationBarTitleDisplayMode(.automatic)
+            
         }
     }
 }
@@ -68,7 +52,7 @@ struct RequestSection: View {
                     Text("Request")
                         .font(.subheadline)
                         .bold()
-                    Text("\(firstData.displayName) + \(sampleData.count - 1) others")
+                    Text("\(invitedPersons.first?.displayName ?? "user") + \(sampleData.count - 1) others")
                         .font(.caption)
                         .foregroundStyle(.secondary)
                 }
@@ -93,7 +77,88 @@ struct RequestSection: View {
 }
 
 
+// MARK: Trusted Person List
 
+struct TrustedPersonList: View {
+    var body: some View {
+        VStack {
+            ForEach(sampleData) { data in
+                TrustedPersonRow(data: data)
+                Divider()
+                    .padding(.leading, 72)
+            }
+            AddParticipantButton()
+        }
+
+        .padding(.vertical, 16)
+        .clipShape(.rect(cornerRadius: 26))
+        .overlay(
+        RoundedRectangle(cornerRadius: 26)
+            .stroke(Color(.systemGray6), lineWidth: 1)
+        )
+    }
+}
+
+// MARK: Trusted Person Row
+
+struct TrustedPersonRow: View {
+    var data: SampleData
+    
+    var body: some View {
+        HStack (spacing: 16) {
+            ZStack {
+                Circle()
+                    .frame(width:40, height: 40)
+                    .foregroundStyle(.quaternary)
+                Image(systemName: "\(data.avatar)")
+                    .font(.body)
+                    .foregroundStyle(Color.white)
+            }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(data.displayName)
+                    .font(.subheadline)
+                    .bold()
+                
+                if data.status == .accepted {
+                    Text(data.icloud)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                    
+                }
+            }
+            
+            if data.status == .invited {
+                Spacer()
+                Text("Invited")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    .padding(.horizontal, 16)
+            }
+            
+            if data.status == .accepted {
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 8)
+    }
+}
+
+// MARK: Add participant Button
+
+struct AddParticipantButton: View {
+    var body: some View {
+        Button {
+            "Add participant"
+        } label: {
+            Text("Add Participant")
+                .padding(.vertical, 8)
+                .padding(.horizontal, 20)
+            Spacer()
+        }
+    }
+}
 
 #Preview {
     TrustedPersonView()
